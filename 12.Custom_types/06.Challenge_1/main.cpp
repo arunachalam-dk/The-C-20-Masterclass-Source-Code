d23f31270374c070af9d834899be2b8bc5d5d9a1
Daniel Gakwaya <wayadn88@gmail.com>
2024-11-01 11:04:02 +0300

    Add tetris related challenge projects to chapters 12 and 15

--- 12.Custom_types/06.Challenge_1/main.cpp
+++ 12.Custom_types/06.Challenge_1/main.cpp
@@ -1,5 +1,6 @@
 /*
   . Setting up the game window using classes.
+    . In the videos, follow the steps laid out in the README file.
 */
 #include "Game.hpp"
 
---
+++ 15.Inheritance_polymorphism/08.Challenge/main.cpp
@@ -0,0 +1,10 @@
+/*
+  . Gameboard.
+    . Break this in as many sub-projects as possible to make it understandable.
+*/
+#include "Game.hpp"
+
+int main() {
+  Game game;
+  game.run();
+}
