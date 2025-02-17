1a36551b78c041a8d23d02b140f4f1fd0fced2d8
Daniel Gakwaya <wayadn88@gmail.com>
2025-02-17 15:26:37 +0300

    Break up the project into cmake subdirs

---
+++ 12_1.Tetris_project/06.Subdirs/src/main.cpp
@@ -0,0 +1,38 @@
+/*
+   
+    . Extract some noisy code from the utilities module into some classes: 
+        . Game
+        . GameState
+        . BoardEntity
+
+    . Next: 
+        . Restructure the project into something like this : 
+            /Root folder
+                ├── CMakeLists.txt
+                ├── assets/
+                │   ├── fonts/
+                │   │   └── arial.ttf
+                │   └── textures/
+                ├── src/
+                │   ├── CMakeLists.txt
+                │   ├── main.cpp
+                │   ├── game/
+                │   │   ├── Game.ixx
+                │   │   └── GameState.ixx
+                │   ├── board/
+                │   │   ├── Board.ixx
+                │   │   └── BoardEntity.ixx
+                │   ├── pieces/
+                │   │   ├── Tetromino.ixx
+                │   │   └── TetrominoFactory.ixx
+                │   └── utils/
+                │       └── Utils.ixx
+
+*/
+import game;
+
+int main() {
+    Game game;
+    game.run();
+    return 0;
+}
